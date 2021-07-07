41b8d8090707c96c62656da877d52d779e68ae05
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-07-07 21:47:41 +0300

    Rename : themes to skins | New : skins

---
+++ assets/js/skins.js
@@ -0,0 +1,61 @@
+'use strict'
+
+
+
+const SKINS = {
+  'default': {
+    snake: {
+      headColor: '#333333',
+      bodyColor: '#555555',
+    },
+
+    food: {
+      color: '#D83A56',
+    },
+    ground: {
+      color: '#66DE93',
+    },
+  },
+
+  'black-gold': {
+    snake: {
+      headColor: '#FF8303',
+      bodyColor: '#FF8303',
+    },
+
+    food: {
+      color: '#F0E3CA',
+    },
+    ground: {
+      color: '#333333',
+    },
+  },
+
+  'palenight': {
+    snake: {
+      headColor: '#21094E',
+      bodyColor: '#511281',
+    },
+
+    food: {
+      color: '#4CA1A3',
+    },
+    ground: {
+      color: '#A5E1AD',
+    },
+  },
+
+  'palenight-reverse': {
+    snake: {
+      headColor: '#4CA1A3',
+      bodyColor: '#511281',
+    },
+
+    food: {
+      color: '#A5E1AD',
+    },
+    ground: {
+      color: '#21094E',
+    },
+  },
+}
